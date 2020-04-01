cb395df5f2a52fbfc77aa67e73c75c6ca4076dbf
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-04-02 00:55:21 +0530

    updated tests

--- nxworkspace/apps/ecommerce/src/app/app.component.spec.ts
+++ nxworkspace/apps/ecommerce/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ProductComponent } from './product/product.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      declarations: [AppComponent, ProductComponent]
     }).compileComponents();
   }));
 
@@ -19,7 +20,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecommerce');
+    expect(app.title).toEqual('ecommerce with Nx');
   });
 
   it('should render title', () => {
@@ -27,7 +28,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to ecommerce!'
+      'Product List'
     );
   });
 });
